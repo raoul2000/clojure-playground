2619d6584b7b4d05f6c0744e43f973eefa28586e
raoul2000 <manu34@free.fr>
2021-04-28 23:41:22 +0200

    exercism : clock wip

--- exercism/clojure/clock/src/clock.clj
+++ exercism/clojure/clock/src/clock.clj
@@ -16,7 +16,6 @@
   )
 
 (comment
-  (= [1 2] '(1 2) #{1 2})
   (clock->string (clock 12 00))
   (clock 24 00)
   (clock 25 00)
@@ -28,6 +27,7 @@
   (clock 201 3001)
   (clock 72 8640)
   (clock -1 15)
+  (clock 1 15)
 
   (clock -25 -160)
   ; 20:20
