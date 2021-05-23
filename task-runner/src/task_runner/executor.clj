c704b080a6aded49b59517fbe820628adfac2a15
raoul2000 <manu34@free.fr>
2021-05-24 00:16:44 +0200

    rtask-runner: .scheduleWithFixedDelay

--- task-runner/src/task_runner/executor.clj
+++ task-runner/src/task_runner/executor.clj
@@ -40,11 +40,83 @@
     ;;(println "executor service stopped")
     ))
 
-(comment
-  (go3)
-  (go2)
-  (f1))
+;; ----------------------------------------------------
+
+(def waiting-msg "Waiting on threads to finish...")
+(defn func-1 [msg]
+  (let [sleep-time (+ 1000 (rand-int 4000))]
+    (println "task " msg " - sleep " sleep-time)
+    (Thread/sleep sleep-time)
+    (println (.getName (Thread/currentThread)) "finishing")
+    (Thread/sleep 20)
+    #{:sleep sleep-time :msg msg}))
+
+(def done-msg    "All threads joined. Exiting.")
+
+;; Java ExecutorService, invokeAll
+
+(defn go4 []
+  (let [tp (Executors/newCachedThreadPool)
+        threads (repeat 5 (partial func-1 "1"))]
+    (println waiting-msg)
+    (.invokeAll tp threads)
+    (println done-msg)))
+
+(defn go5 []
+  (let [tp (Executors/newCachedThreadPool)]
+    (println waiting-msg)
+    (.submit tp (partial func-1 "go-5"))
+    (.shutdown tp)
+    (.awaitTermination tp 4  TimeUnit/SECONDS)
+    (println done-msg)))
+
+(defn go6 []
+  (let [func  (partial func-1 "go-6")
+        stp  (Executors/newScheduledThreadPool 3)]
+    (println waiting-msg)
+    (.scheduleWithFixedDelay stp func 0 1 TimeUnit/SECONDS)
+    (Thread/sleep 2)
+    (.shutdown stp)
+    (.awaitTermination stp 4 TimeUnit/SECONDS)
+    (println done-msg)))
+
+(defn go7 []
+  (let [all-func (repeat 5 (partial func-1 "go-7"))
+        stp (Executors/newScheduledThreadPool 3)
+        fut (map #(.scheduleWithFixedDelay stp % 0 250 TimeUnit/MILLISECONDS)
+                 all-func)]
+    (println waiting-msg)
+    (println (count fut))
+    (Thread/sleep 5000)
+    (.shutdown stp)
+    (.awaitTermination stp 4  TimeUnit/SECONDS)))
+
+;; interactive ---------------------------------------------------
+;; from repl run (sched #(println "A"))
+;; to stop run stop-stp
+(def stp  (Executors/newScheduledThreadPool 3))
+
+(defn sched [t]
+  (.scheduleWithFixedDelay stp t 0 250 TimeUnit/MILLISECONDS))
+
+(defn stop-stp []
+  (.shutdown stp)
+  (println "shutting down ...")
+  (.awaitTermination stp 2 TimeUnit/SECONDS)
+  (println "shutdown!"))
 
 
+;; ---------------------------------
+;; wip
+(def task-1-run (atom false))
+(defn toggle-task-1-run []
+  (swap! task-1-run not))
 
+(defn task-1)
+
+(defn task-1 []
+  (for [round (range)
+        :while @task-1-run]
+    (Thread/sleep 1000)
+    (println round)))
 
