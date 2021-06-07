261a11bda81356f75089b02964717b9d08a9762a
raoul2000 <manu34@free.fr>
2021-06-07 23:52:56 +0200

    exercism: isbn - submit solution 2

--- exercism/clojure/isbn-verifier/src/isbn_verifier.clj
+++ exercism/clojure/isbn-verifier/src/isbn_verifier.clj
@@ -1,24 +1,20 @@
-(ns isbn-verifier)
+(ns isbn-verifier
+  (:require [clojure.string :as s]))
 
-(defn isbn-char-to-int
+(defn isbn-char->int
   [c]
   (if (= \X c)
     10
     (Character/digit c 10)))
 
-(defn isbn-checksum [r k v]
-  (+ r (* (inc k) (isbn-char-to-int v))))
-
 (defn isbn? [isbn]
-  (let [no-dash  (filter #(not (= \- %)) isbn)
-        len      (count no-dash)
-        valid    (and (re-matches #"^(?:(\d+)\-?)+[X\d]$" isbn)
-                      (= 10 len))]
-    (if valid
-      (= 0 (mod
-            (->> no-dash
-                 reverse
-                 (into [])
-                 (reduce-kv isbn-checksum 0)) 
-            11))
-      false)))
+  (let [no-dash (s/replace isbn "-" "")]
+    (boolean
+     (and
+      (re-matches #"^\d{9}[X\d]$" no-dash)
+      (->> no-dash
+           (map isbn-char->int)
+           (map * (range 10 0 -1))
+           (apply +)
+           (#(mod % 11))
+           zero?)))))
