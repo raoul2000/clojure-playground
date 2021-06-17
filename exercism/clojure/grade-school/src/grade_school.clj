61ecb0f3887cd09cbd1686783d4692c16234af48
raoul2000 <manu34@free.fr>
2021-06-18 00:12:21 +0200

    exercism: grade-school - begin

---
+++ exercism/clojure/grade-school/src/grade_school.clj
@@ -0,0 +1,27 @@
+(ns grade-school)
+
+(def db {})
+
+(defn grade [school grade]  ;; <- arglist goes here
+   )
+
+
+(defn add [school name grade]  ;; <- arglist goes here
+    
+)
+
+(defn sorted [school]  ;; <- arglist goes here
+    ;; your code goes here
+)
+(comment
+  ;; convert a string to a keyword
+  (let [s "12"]
+    (keyword (str s)))
+  
+  ;; ..but not needed as an int can be used as map keu
+  (def db { 1 ["a" "b" "c"]
+           3 ["e" "r" "a"]})
+  
+
+  ;;
+  )
