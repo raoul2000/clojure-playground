da4e4ecf98c63a6116ac8e7381e892b1f5404f97
raoul2000 <manu34@free.fr>
2021-09-24 00:09:55 +0200

    exercism: say  - iteration 5

--- exercism/clojure/say/src/say.clj
+++ exercism/clojure/say/src/say.clj
@@ -1,67 +1,61 @@
 (ns say
   (:require [clojure.string :refer [join trimr]]))
 
-(def spec {0 "zero"    1  "one"  2  "two"  3  "three", 
-           4 "four"    5  "five" 6  "six"  7  "seven", 
-           8 "eight"   9  "nine" 10 "ten"  11 "eleven", 
-           12 "twelve" 14 "fourteen"})
-
-(def root  ["twen" "thir" "for" "fif" "six" "seven" "eigh" "nine"])
+(def spec {0 "zero"     1  "one"       2  "two"      3  "three"
+           4 "four"     5  "five"      6  "six"      7  "seven"
+           8 "eight"    9  "nine"      10 "ten"      11 "eleven"
+           12 "twelve"  13 "thirteen"  14 "fourteen" 15 "fifteen"
+           16 "sixteen" 17 "seventeen" 18 "eighteen" 19 "ninteen"
+           20 "twenty"  30 "thirty"    40 "forty"    50 "fifty"
+           60 "sixty"   70 "seventy"   80 "eighty"   90 "ninety"})
 
 (defn say<20
   "say n when n < 20, nil otherwise
-   ex: one, two, ...ten, eleven, twelve, thirteen, ... nineteen"
+  ex: one, two, ...ten, eleven, twelve, thirteen, ... nineteen"
   [n]
-  (or
-   (spec n)
-   (when-let [s (root (- n 12))]
-     (str s "teen"))))
+  (when (< n 20) (spec n)))
 
 (defn say-the-ten
   "say the ten of n when 19 < n < 100, nil otherwise
   ex :  twenty, thirty, ... ninety"
   [n]
-  (when-let [s (root (- (quot n 10) 2))]
-    (str s "ty")))
+  (when (< 19 n 100)
+    (spec (* 10 (quot n 10)))))
 
-(defn  say<99
-  "say n when n < 99, nil otherwise
-   ex: one, twenty-one, fifty, ... ninety-nine"
+(defn  say<100
+  "say n when n < 1000, nil otherwise
+  ex: one, twenty-one, fifty, ... ninety-nine"
   [n]
-  (condp > n
-    20   (say<20  n)
-    100  (str
-          (say-the-ten n)
-          (let [r (rem n 10)]
-            (when (pos? r) (str "-" (say<20 r)))))
-    nil))
+  (cond
+    (< n 20)   (say<20  n)
+    (< n 100)  (str
+                (say-the-ten n)
+                (let [r (rem n 10)]
+                  (when (pos? r) (str "-" (say<20 r)))))))
 
-(defn say<999
-  "say n when n < 999, nil otherwise
-   ex: three hundred tenwty-five ...nine hundred ninty-nine"
+(defn say<1000
+  "say n when n < 1000, nil otherwise
+  ex: three hundred tenwty-five ...nine hundred ninty-nine"
   [n]
-  (condp > n
-    99   (say<99 n)
-    1000 (str
-          (say<20 (quot n 100))
-          " hundred"
-          (let [r (rem n 100)]
-            (when (pos? r) (str " " (say<99 r)))))
-    nil))
+  (cond
+    (< n 99)   (say<100 n)
+    (< n 1000) (str
+                (say<20 (quot n 100))
+                " hundred"
+                (let [r (rem n 100)]
+                  (when (pos? r) (str " " (say<100 r)))))))
 
 (defn split-by-3-digits
   "split num into max of 3 digits groups
-   ex: 1 223 656 => (656 223 1)"
+  ex: 1 223 656 => (656 223 1)"
   [num]
-  (->> [num 0]
-       (iterate    (fn [[n _]] [(quot n 1000) (rem n 1000)]))
+  (->> (iterate    (fn [[n _]] [(quot n 1000) (rem n 1000)]) [num 0])
        (take-while (fn [[n r]] (not= n r 0)))
        rest
        (map last)))
 
 (defn add-scale-words [xs]
-  (->>  ["" "thousand" "million"  "billion" "trillion"]
-        (map vector xs)
+  (->>  (map vector xs ["" "thousand" "million"  "billion" "trillion"])
         (filter (comp pos? first))
         reverse
         flatten))
@@ -69,10 +63,10 @@
 (defn number [num]
   (cond
     (not (< -1 num 999999999999))   (throw (IllegalArgumentException.))
-    (< num 999)                     (say<999 num)
+    (< num 999)                     (say<1000 num)
     :else (->> num
                split-by-3-digits
                add-scale-words
-               (map #(if (number? %) (say<999 %) %))
+               (map #(if (number? %) (say<1000 %) %))
                (join " ")
                trimr)))
