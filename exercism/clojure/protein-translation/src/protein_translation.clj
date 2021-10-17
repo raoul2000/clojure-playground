788d6c5eb660805f86344658ec464494ae412a8a
raoul2000 <manu34@free.fr>
2021-10-17 23:46:23 +0200

    exercism: protein translation

---
+++ exercism/clojure/protein-translation/src/protein_translation.clj
@@ -0,0 +1,33 @@
+(ns protein-translation)
+
+
+
+(def translate-codon {"AUG" "Methionine"
+                      "UUU" "Phenylalanine"
+                      "UUC" "Phenylalanine"
+                      "UUA" "Leucine"
+                      "UUG" "Leucine"
+                      "UCU" "Serine"
+                      "UCC" "Serine"
+                      "UCA" "Serine"
+                      "UCG" "Serine"
+                      "UAU" "Tyrosine"
+                      "UAC" "Tyrosine"
+                      "UGU" "Cysteine"
+                      "UGC" "Cysteine"
+                      "UGG" "Tryptophan"
+                      "UAA" "STOP"
+                      "UAG" "STOP"
+                      "UGA" "STOP"})
+
+(defn translate-rna [s]
+  (->> (partition 3 s)
+       (map #(apply str %))
+       (map translate-codon)
+       (take-while #(not (= "STOP" %)))))
+
+(comment
+
+  (translate-rna "AUGUUCUAAUGU")
+  ;;
+  )
