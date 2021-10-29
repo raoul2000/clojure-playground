e5165ea0e20fe54f58e88223ba66a36aa5505fdc
raoul2000 <manu34@free.fr>
2021-10-30 00:21:04 +0200

    exercism: etl - submited solution

--- exercism/clojure/etl/src/etl.clj
+++ exercism/clojure/etl/src/etl.clj
@@ -1,19 +1,7 @@
 (ns etl
   (:require [clojure.string :refer [lower-case]]))
 
-
-(defn etl-reducer [m [k vs]]
-  (into m (for [val vs]
-            [(lower-case val) k])))
-
-
 (defn transform [source]
-  (reduce etl-reducer {} source))
-
-
-(comment
-
-  (transform {1 ["APPLE" "ARTICHOKE"], 2 ["BOAT" "BALLERINA"]})
-
-  ;;
-  )
+  (into {} (for [[num words-list] source
+                 word words-list]
+             [(lower-case word) num])))
