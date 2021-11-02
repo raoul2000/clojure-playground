b812d79f41660d0bfd52123e130e1af73938209f
raoul2000 <manu34@free.fr>
2021-11-02 23:26:09 +0100

    exercism: robot-name iteration 1 (transient)

---
+++ exercism/clojure/robot-name/src/robot_name.clj
@@ -0,0 +1,19 @@
+(ns robot-name)
+
+(defn rand-letter []
+  (char (+ 65 (rand-int 26))))
+
+(defn rand-num []
+  (char (+ 48 (rand-int 10))))
+
+(defn rand-name []
+  (apply str (concat (repeatedly 2 rand-letter) (repeatedly 3 rand-num))))
+
+(defn robot [] 
+  (transient {:name (rand-name)}))
+
+(defn robot-name [robot] 
+  (:name robot))
+
+(defn reset-name [robot]
+  (assoc! robot :name (rand-name)))
