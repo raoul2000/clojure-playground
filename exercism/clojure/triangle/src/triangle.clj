6276f4f3c6a77474787f7d737bfcb5adf559b459
raoul2000 <manu34@free.fr>
2021-11-06 23:55:48 +0100

    exercism: triangle begin

---
+++ exercism/clojure/triangle/src/triangle.clj
@@ -0,0 +1,18 @@
+(ns triangle)
+
+(defn is-valid? [] ;; <- arglist goes here
+  ;; your code goes here
+)
+
+(defn equilateral? [a b c] ;; <- arglist goes here
+  (= a b c)
+)
+
+(defn isosceles? [] ;; <- arglist goes here
+  ;; your code goes here
+)
+
+
+(defn scalene? [] ;; <- arglist goes here
+  ;; your code goes here
+)
