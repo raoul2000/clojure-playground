ddbfa13653f83eda60d2aad25d915d51a8bcbce3
raoul2000 <manu34@free.fr>
2021-11-10 00:11:52 +0100

    exercism: triangle complete solution

--- exercism/clojure/triangle/src/triangle.clj
+++ exercism/clojure/triangle/src/triangle.clj
@@ -1,40 +1,25 @@
 (ns triangle)
 
-(defn is-valid? [a b c]
-  (let [v [a b c]
-        m (apply max v)]
+(defn valid-triangle? [a b c]
+  (let [sizes (sort [a b c])]
     (and
-     (not-every? zero? [a b c])
-     (< (- m (apply + v)) 0))))
-
-(comment
-  (is-valid? 1 3 1)
-  (is-valid? 0 0 0)
-  ;;
-  )
+     (not-every? zero? sizes)
+     (> (+ (first sizes) (second sizes)) (last sizes)))))
 
 (defn equilateral? [a b c]
   (and
-   (is-valid? a b c)
+   (valid-triangle? a b c)
    (= a b c)))
 
 (defn isosceles? [a b c]
-  (when (is-valid? a b c)
-    (or
+  (and
+   (valid-triangle? a b c)
+   (or
      (equilateral? a b c)
-     (= a b)
-     (= a c)
-     (= b c))))
-
-(comment
-  (isosceles? 7 3 2)
-  ;;
-  )
+     (= 2 (count (into #{} [a b c]))))))
 
 (defn scalene? [a b c]
-  (not (isosceles? a b c)))
+  (and
+   (valid-triangle? a b c)
+   (not (isosceles? a b c))))
 
-(comment
-  (scalene? 7 3 2)
-  ;;
-  )
