065ba92102c7570e79376c5964a6c90925a80a7f
raoul2000 <manu34@free.fr>
2022-03-15 00:30:33 +0100

    exercism: sum of multiple - after refactor (but submit previous iteration)

--- exercism/clojure/sum-of-multiples/src/sum_of_multiples.clj
+++ exercism/clojure/sum-of-multiples/src/sum_of_multiples.clj
@@ -1,16 +1,32 @@
 (ns sum-of-multiples)
 
 
-(defn multiple? [xs]
+(defn multiple?-1 [xs]
   (fn [n]
     (some #(zero? (rem n %)) xs)))
 
-(defn sum-of-multiples [xs max]
+(defn sum-of-multiples-1 [xs max]
   (->> (range 0 max)
-       (filter (multiple? xs) )
+       (filter (multiple?-1 xs))
        (apply +)))
+
+;; refactor
+
+;; less verbose but maybe also less readable...
+(defn sum-of-multiples [xs max]
+  (let [multiple? (fn [n] (some #(zero? (rem n %)) xs))]
+    (reduce #(+ %1 (if (multiple? %2) %2 0)) 0 (range 0 max))))
 
 (comment
   (sum-of-multiples [3 5] 20))
+
+;; notes
+;; Suggestion: multiplies-up-to is
+;; (range n upper-bound n))
+;; see https://exercism.org/tracks/clojure/exercises/sum-of-multiples/solutions/jumarko
+
+;; using transduce
+;; see https://exercism.org/tracks/clojure/exercises/sum-of-multiples/solutions/dkinzer
+
 
 
