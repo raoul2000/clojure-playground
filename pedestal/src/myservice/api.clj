5ce4b345133d21283d8956edef6ec4de09bf1a93
raoul2000 <manu34@free.fr>
2022-03-19 23:33:14 +0100

    pedestal: greetings with query params

--- pedestal/src/myservice/api.clj
+++ pedestal/src/myservice/api.clj
@@ -3,17 +3,16 @@
             [io.pedestal.http.route :as route])
   (:gen-class))
 
-
-
-
+(defn ok [body]
+  {:status 200 :body body})
 
 (defn respond-hello [request]
-  {:status 200 :body "Hello, world!"})
+  (let [name (get-in request [:query-params :name] "stranger")]
+    (ok (str "hello, " name))))
 
 (def routes
   (route/expand-routes
    #{["/greet" :get respond-hello :route-name :greet]}))
-
 
 (def service-map
   {::http/routes routes
@@ -23,15 +22,17 @@
 (defn start []
   (http/start (http/create-server service-map)))
 
+(defn start-server []
+  (http/start (http/create-server
+               (assoc service-map
+                      ::http/join? false))))
+
 ;; interactive development ----------------------------------------------
 
 (defonce server (atom nil))
 
 (defn start-dev []
-  (reset! server
-          (http/start (http/create-server
-                       (assoc service-map
-                              ::http/join? false)))))
+  (reset! server (start-server)))
 
 (defn stop-dev []
   (http/stop @server))
@@ -40,10 +41,7 @@
   (stop-dev)
   (start-dev))
 
-
-
-
-
+;; CLI ----------------------------------------------------------------
 
 (defn greet
   "Callable entry point to the application."
@@ -51,6 +49,7 @@
   (println (str "Hello, " (or (:name data) "World") "!")))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (greet {:name (first args)}))
+  (greet {:name (first args)})
+  (println (str "now starting server at port " (service-map ::http/port)))
+  (start-server))
