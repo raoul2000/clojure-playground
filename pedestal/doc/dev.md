7513fc23b12602721a42306a8249a9705124b0fe
raoul2000 <manu34@free.fr>
2022-03-20 18:29:03 +0100

    pedestal: greetings with content negociation, logger and some dev doc + tests

--- pedestal/doc/dev.md
+++ pedestal/doc/dev.md
@@ -12,3 +12,42 @@ clj꞉user꞉> 
 - use `stop-dev` and `restart` in the same way
 
 Note that same effect can be obtained using *Calva* and evaluation of the source code.
+
+# Routes in dev
+
+
+## Display Routes
+
+Use `route/print-routes`to display routes in an easy to read format. In particular this will output the route Id automatically generated when none is provided.
+
+Example:
+```clojure
+;; define our routes. See that "/echo" has no route ID when "/greet" has one (:greet)
+(def routes
+  (route/expand-routes
+   #{["/greet" :get [coerce-body content-neg-intc respond-hello]  :route-name :greet]
+     ["/echo"  :get [coerce-body content-neg-intc echo] ]}))
+
+(route/print-routes routes)
+;; output - the route id for "/echo" is :myservice.api/echo
+[:get /greet :greet]
+[:get /echo :myservice.api/echo]    
+```
+
+## Create URL for route
+
+Function `route/url-for-routes` returns the URL for a route given its ID.
+Example:
+```clojure
+((route/url-for-routes routes)  :myservice.api/echo)
+"/echo"
+```
+
+# Response Test
+
+To test what is the response to a given request :
+
+```clojure
+(test/response-for (:io.pedestal.http/service-fn @server) :get "/echo"
+                   :headers {"Accept" "application/edn"})
+```                   
--- pedestal/src/myservice/api.clj
+++ pedestal/src/myservice/api.clj
@@ -1,18 +1,94 @@
 (ns myservice.api
-  (:require [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route])
+  (:require [clojure.data.json :as json]
+            [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.http.content-negotiation :as conneg]
+            [io.pedestal.test :as test]
+            [io.pedestal.log :as log])
   (:gen-class))
 
+
 (defn ok [body]
-  {:status 200 :body body})
+  {:status 200
+   :body body})
+
+(defn not-found []
+  {:status 404 :body "Not found or forbidden name\n"})
+
+;; /greet -------------------------------------------------
+
+(defn greeting-for [name]
+  (cond
+    (nil? name)            "hello, stranger"
+    (#{"bob" "max"} name)  nil
+    :else                  (str "hello, " name)))
 
 (defn respond-hello [request]
-  (let [name (get-in request [:query-params :name] "stranger")]
-    (ok (str "hello, " name))))
+  (let [name (get-in request [:query-params :name])
+        resp {:reply (greeting-for name)}]
+    (if resp
+      (ok resp)
+      (not-found))))
 
+;; /echo -------------------------------------------------
+
+;; define a basic 'echo' interceptor
+(def echo
+  {:name ::echo
+   :enter #(assoc % :response (ok (:request %))) ;; anonymous function to update the :response
+                                                 ;; key in the context map
+   })
+
+(def supported-types ["text/html" "application/edn" "application/json" "text/plain"])
+;; create a content negociator interceptor for the given list
+;; of supported content types
+(def content-neg-intc (conneg/negotiate-content supported-types))
+
+
+(defn accepted-type
+  "returns the accepted content type from the context map or `text/plain` if not set"
+  [context]
+  (get-in context [:request :accept :field] "text/plain"))
+
+(defn transform-content
+  "Converts and returns *body* into the given *content-type*"
+  [body content-type]
+  (log/info "transform content" content-type)
+  (case content-type
+    "text/html"        body
+    "text/plain"       body
+    "application/edn"  (pr-str body)
+    "application/json" (json/write-str body)))
+
+(defn coerce-to
+  "Updates and returns a response map given a *content-type*. The *response* body
+   is coerced to the *content-type* and the Content-Type header is assigned the right value"
+  [response content-type]
+  (-> response
+      (update :body transform-content content-type)
+      (assoc-in [:headers "Content-Type"] content-type)))
+
+(defn no-content-type? 
+  "Returns TRUE if the response header map doesn't contain any *Content-Type* key"
+  [context]
+  (nil? (get-in context [:response :headers "Content-Type"])))
+
+;; interceptor with only a 'leave' handler dedicated to be executed last to
+;; convert the response body into the best match for accepted content type
+;; as it was computed by content negociation interceptor (content-neg-intc)
+(def coerce-body
+  {:name ::coerce-body
+   :leave (fn [context]
+            (cond-> context
+              (no-content-type? context) (update-in [:response] coerce-to (accepted-type context))))})
+
+
+;; to test route in the REPL
+;; (route/try-routing-for hello/routes :prefix-tree "/greet" :get)
 (def routes
   (route/expand-routes
-   #{["/greet" :get respond-hello :route-name :greet]}))
+   #{["/greet" :get [coerce-body content-neg-intc respond-hello]  :route-name :greet]
+     ["/echo"  :get [echo] :route-name :echo]}))
 
 (def service-map
   {::http/routes routes
@@ -41,6 +117,11 @@
   (stop-dev)
   (start-dev))
 
+(comment
+  (test/response-for (:io.pedestal.http/service-fn @server) :get "/echo"
+                     :headers {"Accept" "application/json"})
+  ;;
+  )
 ;; CLI ----------------------------------------------------------------
 
 (defn greet
