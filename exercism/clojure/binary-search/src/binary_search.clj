341547f44ee3dc94faff8d8eac084dcb52f70be8
raoul2000 <manu34@free.fr>
2022-04-02 01:37:35 +0200

    exercism: binary_search - final solution

--- exercism/clojure/binary-search/src/binary_search.clj
+++ exercism/clojure/binary-search/src/binary_search.clj
@@ -1,27 +1,13 @@
 (ns binary-search)
 
-;; start with a list : [1 2 5 8 10 14]
-;; and a search key: 2
-;; find the middle index : (/ (count list) 2) = 3
-;; get value at position 3 = 8
-;; test 8
-;; - 2 = 8 false
-;; - 2 > 8 false
-;; - 2 < 8 true get first half = [1 2 3 8]
-
-
-
 (defn middle [xs]
-  (quot (count xs) 2))
-
-(comment
-  (middle [1 2]))
+  (-> xs count (quot 2)))
 
 (defn split [xs]
   (let [middle-idx (middle xs)]
-    (vector (nth    xs middle-idx)
-            (subvec xs 0 middle-idx)
-            (subvec xs (inc middle-idx)))))
+    (vector (nth    xs middle-idx)           ;; the pivot item
+            (subvec xs 0 middle-idx)         ;; the left sub-array
+            (subvec xs (inc middle-idx)))))  ;; the right sub-array 
 
 (comment
   (split [1 2 3 4])
@@ -32,30 +18,25 @@
   ;;
   )
 
-(defn item-val [pair] (last  pair))
-(defn item-idx [pair] (first pair))
+(defn search-for [searched-val coll]
+  (loop [indexed-vec (vec (map-indexed vector coll))] ;; preserve initial item [index value]
+    (cond
+      (= 1 (count indexed-vec))
+      (let [[[remaining-item-idx remaining-item-val]] indexed-vec]
+        (if (= searched-val remaining-item-val)
+          remaining-item-idx
+          (throw (Exception. "not found"))))
 
-(defn search-for [val coll]
-  (let [found-item? #(= val (item-val %))]
-    (loop [xs (vec (map-indexed vector coll))]
-      (cond
-        (= 1 (count xs)) (if (found-item? (first xs))
-                           (item-idx (first xs))
-                           (throw (Exception. "not found")))
-        :else (let [[pivot left-part right-part] (split xs)]
-                (if (= val (item-val pivot))
-                  (item-idx pivot)
-                  (recur (if (> val (item-val pivot))
-                           right-part
-                           left-part))))))))
+      :else
+      (let [[[pivot-idx pivot-val] left-sub-array right-sub-array] (split indexed-vec)]
+        (cond
+          (= searched-val pivot-val) pivot-idx
+          (> searched-val pivot-val) (recur right-sub-array)
+          :else                      (recur left-sub-array))))))
 
 (comment
-  (vec (map-indexed vector [:a :b]))
+
   (search-for 4 [4])
   (search-for 4 [1 2 3])
-  (search-for 2 [1 2 3])
-
-
-  ;;
-  )
+  (search-for 2 [1 2 3])  )
 
