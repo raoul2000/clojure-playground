0f9f260a029630e1c51cdd597f24d306471fe276
raoul2000 <manu34@free.fr>
2022-04-03 18:06:41 +0200

    exercism: binary-search-tree complete solution

--- exercism/clojure/binary-search-tree/src/binary_search_tree.clj
+++ exercism/clojure/binary-search-tree/src/binary_search_tree.clj
@@ -1,49 +1,35 @@
 (ns binary-search-tree)
 
-;; let's try to reprosent a node as a map with 3 keys
-;; - :value the node value
-;; - :left  the left subtree
-;; - :right  the right subtree
+;; let's try to represent a node as a 3 items arrays
+;; - first item : left subtree or nil
+;; - second item : the value
+;; - third item : the right substree or nil
 
+(defn singleton [value] [nil value nil])
+(def value second)
+(def left  first )
+(def right last  )
 
-(defn value [node]
-  (:value node))
-
-(defn singleton [value]
-  {:value value})
-
-(defn left [node]
-  (:left node))
-
-(defn right [node]
-  (:right node))
-
-(defn select-subtree [val node]
-  (cond
-    (> val (value node))  [:right (right node)]
-    :else                 [:left  (left node)]))
+(defn select-subtree
+  [val node]
+  (if (> val (value node))
+    [2 (right node)]     ;; returns right selector key and subtree
+    [0 (left node)]))    ;; returns left selector key and subtree
 
 (defn insert [value node]
   (let [[sel-key sel-branch] (select-subtree value node)]
     (assoc node sel-key (cond
                           (nil? sel-branch) (singleton value)
-                          :else             (recur value sel-branch)))))
+                          :else             (insert value sel-branch)))))
 
 (defn from-list [xs]
-  (reduce (fn [res val]
-            (insert val res))
+  (reduce (fn [res val] (insert val res))
           (singleton (first xs))
           (rest xs)))
 
-
 (defn to-list [node]
-  (when node
-    (-> (vector (to-list (:left node))
-                (:value node)
-                (to-list (:right node)))
-        (remove nil?)
-        flatten
-        (into []))))
+  (->> (flatten node)
+       (remove nil?)))
 
 (comment
   (from-list [5 3  1 2 3])
