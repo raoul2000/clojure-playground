cdcf1d72d71e290c46c56cb317511d002251f1a2
raoul2000 <manu34@free.fr>
2022-05-08 23:11:22 +0200

    exercism: clock in progress

--- exercism/clojure/clock/src/clock.clj
+++ exercism/clojure/clock/src/clock.clj
@@ -1,5 +1,25 @@
 (ns clock)
 
+;; Following implementation base on Java LocalTime type is working o;
+;; meaning that all trest pass. However for some reason, the exercism editor
+;; doesn't accept it.
+
+;; (defn clock->string [clock] ;; <- arglist goes here
+;;   (format "%02d:%02d"
+;;           (.getHour clock)
+;;           (.getMinute clock)))
+
+;; (defn clock [hours minutes] ;; <- arglist goes here
+;;   (java.time.LocalTime/of hours minutes))
+
+;; (defn add-time [clock time] ;; <- arglist goes here
+;;   (if (neg-int? time)
+;;     (.minusMinutes clock (Math/abs time))
+;;     (.plusMinutes clock time)))
+
+;; let's implement another solution
+;;
+
 (defn clock->string [clock] ;; <- arglist goes here
   (format "%02d:%02d"
           (first clock)
@@ -16,7 +36,16 @@
   )
 
 (comment
+  
+(+ (* 3 60) 15)
+  (mod (- 60 160) 60)
+  ()
+  ;;
+  )
+
+(comment
   (clock->string (clock 12 00))
+  (clock 8 0)
   (clock 24 00)
   (clock 25 00)
   (clock 100 00)
@@ -32,3 +61,5 @@
   (clock -25 -160)
   ; 20:20
   (clock -121 -5810))
+
+
