b975189000bc15caedd9db32c68ca8e3874237f8
raoul2000 <manu34@free.fr>
2022-06-11 14:27:41 +0200

    exercism: kindergarden submitted solution

--- exercism/clojure/kindergarten-garden/src/kindergarten_garden.clj
+++ exercism/clojure/kindergarten-garden/src/kindergarten_garden.clj
@@ -1,22 +1,43 @@
 (ns kindergarten-garden
-  (:require [clojure.string :refer [split]]))
+  (:require [clojure.string :refer [split lower-case]]))
 
-(defn garden [] ;; <- arglist goes here
-  ;; your code goes here
-)
+;; [:a :b :c :d 1 2 3 4]  --> [ [:a :b 1 2] [:c :d 3 4]] --> {:alice [:a :b 1 2]
+;;                                                            :bob   [:c :d 3 4]}
 
-(comment
-  (def s "VVCCGG\nVVCCGG")
-  (split s #"\n")
-  (def l1 (map (partial into []) (split s #"\n")))
+(def class-kids ["alice" "bob" "charlie" "david" "eve" "fred" "ginny" "harriet" "ileana" "joseph" "kincaid" "larry"])
+(def plant-name {\V :violets, \R :radishes, \C :clover, \G :grass})
 
-  (map (fn [a b ] (vector a b )) [:a :b :c] [1 2 3])
-  (map (fn [a b ] (vector a b )) [:a :b :c] [1 2 3])
+(defn code-row->plant-name 
+  "Converts a list of plants code into a list of plants keyword.
+   (example: [\"VC\"] --> [:violets :clover])"
+  [^String code]
+  (mapv plant-name code))
 
-  (take 2 [:a :b :c :d])
-  (apply (comp take 2) [:a :b :c :d] [1 2 3 4])
-  (juxt (comp take 2) [:a :b :c :d] [1 2 3 4])
-  
+(def keywordize (comp keyword lower-case))
+
+(defn garden-1
+  ([plants]
+   (garden-1 plants class-kids))
+  ([plants kids]
+   (let [kid-list (map keywordize (sort kids))]
+     (->> (split plants #"\n")
+          (map code-row->plant-name)
+          (map (partial partition 2))
+          (#(map concat (first %) (last %)))
+          (interleave kid-list)
+          (apply hash-map)))))
+
+;; improve solution : use zipmap
+
+(defn garden
+  ([plant-cups]
+   (garden plant-cups class-kids))
+  ([plant-cups kids]
+   (let [kid-list (mapv keywordize (sort kids))]
+     (->> (split plant-cups #"\n")
+          (map code-row->plant-name)
+          (map (partial partition 2))
+          (#(map concat (first %) (last %)))
+          (zipmap kid-list)))))
 
 
-  )
