27164d8324499a7af3b0ab7daaa184dad642d7e7
raoul2000 <manu34@free.fr>
2022-07-03 23:50:08 +0200

    exercism: dominoes - getting closer to a solution using zippers

--- exercism/clojure/dominoes/src/zip_play.clj
+++ exercism/clojure/dominoes/src/zip_play.clj
@@ -155,7 +155,7 @@
   ;;
   )
 
-(comment
+
   ;; let's go back to the Dominoes exercism
   ;; - a domino represented as a vector of 2 integers
   ;; - 2 dominoes can connect to each other if they have at least one value in common
@@ -165,58 +165,98 @@
   ;;      Example: [ [1 2] [2 4] [4 4] [4 2]]
   ;; - a domino chain is valid if the 2 not connected values are the same
 
-  (defn flip [[v1 v2]]  [v2 v1])
+(defn flip [[v1 v2]]  [v2 v1])
+
+
+(defn can-connect-no-flip [[_ a2] [b1 _]] (= a2 b1))
+
+(comment
   (flip [2 3])
-
-  (defn same-domino [a b]
-    (or (= a b)
-        (= a (flip b))))
-  (same-domino [1 2] [1 2])
-  (same-domino [1 2] [2 1])
-  (same-domino [1 2] [1 3])
-
-  (defn can-connect-no-flip [[_ a2] [b1 _]] (= a2 b1))
-
   (can-connect-no-flip [1 2] [2 1])
   (can-connect-no-flip [1 2] [3 1])
-  (can-connect-no-flip [1 2] [3 4])
+  (can-connect-no-flip [1 2] [3 4]))
 
-  (defn connect
-    "Given 2 dominoes, returns nil if they can't connect, or b or (flip b) 
-     if they can connect"
-    [a b]
-    (if (can-connect-no-flip a b)
-      b
-      (let [flipped-b (flip b)]
-        (when (can-connect-no-flip a flipped-b)
-          flipped-b))))
+(defn connect
+  "Given 2 dominoes, where the first one is 'fixed' (i.e can't flip) and the second one is 
+     a candidate for connection, returns nil if they can't connect or the candidate dominoe
+     possibly fillped to connect to the first one."
+  [fixed-domino domino]
+  (if (can-connect-no-flip fixed-domino domino)
+    domino
+    (let [flipped-b (flip domino)]
+      (when (can-connect-no-flip fixed-domino flipped-b)
+        flipped-b))))
 
+(comment
   (connect [1 2] [2 3])
   (connect [1 2] [3 2])
-  (connect [1 2] [3 4])
+  (connect [1 2] [3 4]))
 
-  (defn group-by-connect [tail-domino dominoes]
-    (reduce (fn [result domino]
-              (if-let [connected-dom (connect tail-domino domino)]
-                (update result :can-connect conj connected-dom)
-                (update result :rejected    conj domino)))
-            {:can-connect [] :rejected []}
-            dominoes))
+(defn group-by-connect [tail-domino dominoes]
+  (reduce (fn [result domino]
+            (if-let [connected-dom (connect tail-domino domino)]
+              (update result :can-connect conj connected-dom)
+              (update result :rejected    conj domino)))
+          {:can-connect [] :rejected []}
+          dominoes))
 
+(comment
   (group-by-connect [1 2] [[1 2] [2 1] [3 4] [4 3]])
   (group-by-connect [1 4] [[1 4] [2 1] [3 4] [4 3]])
-  (group-by-connect [1 4] [[1 4] [2 1] [4 4] [4 3]])
   (group-by-connect [6 7] [[1 4] [2 1] [4 4] [4 3]])
+  (group-by-connect [1 4] [[1 4] [2 1] [4 4] [4 3]]))
 
-  (defn remove-first [i xs]
-    (reduce (fn [result v]
-              (if (:removed result)
-                (update result :list conj i)
-                (do
-                  (assoc result :removed true))))
-            {:removed false
-             :lst []}
-            xs))
+  ;; this is ok but it would be better to get for each match, the matching domino and the remainng dominoes
+  ;; Example:
+  ;; (matches [1 2] [ [1 2] [2 1] [3 4] [4 3]])
+  ;; => [
+  ;;      [ [2 1] 
+  ;;             [[2 1] [3 4] [4 3]] 
+  ;;      ]
+  ;;      [ [2 1] 
+  ;;             [[1 2] [3 4] [4 3]] 
+  ;;      ]
+  ;;   ]
+  ;;]
+
+
+
+(defn remove-domino-by-index [idx indexed-dominoes]
+  (->> (remove (fn [[current-idx _]]
+                 (= current-idx idx))
+               indexed-dominoes)
+       (map last)  ;; ignore first item (the index) only keep domino
+       ))
+
+(comment
+  (remove-domino-by-index 2 [[1 :a] [2 :b] [3 :c]])
+  (remove-domino-by-index 2 [[1 :a] [4 :b] [3 :c]]))
+
+(defn find-possible-connections
+  "Given a fixed domino (can't flip) and a list of dominoes candidates to connect,
+     Returns a vector of all possible connections, where the first item is the possibly flipped
+     domino that connects, and the second item is the list of remaining dominoes.
+     Returns an empty vector when no connection is possible. "
+  [fixed-domino dominoes]
+  (let [indexed-dominoes (map-indexed vector dominoes)]
+    (reduce (fn [result [idx domino]]
+              (if-let [connected-domino (connect fixed-domino domino)]
+                (conj result [connected-domino (remove-domino-by-index idx indexed-dominoes)])
+                result))
+            []
+            indexed-dominoes)))
+
+(comment
+  (find-possible-connections [1 2] [[3 2] [2 1] [3 4] [4 3]])
+  ;; => [  [[2 3] ,  ([2 1] [3 4] [4 3])] 
+  ;;       [[2 1] ,  ([3 2] [3 4] [4 3])]
+  ;; ]
+  (find-possible-connections [1 2] [[2 6] [5 2] [3 4] [4 3]])
+  (find-possible-connections [1 2] [[3 6] [5 4] [3 4] [4 3]])
+  ;; => []
+  )
+
+
   ;; Now lets  prepare our tree
   ;; each node has the following shape
   ;; {
@@ -227,17 +267,19 @@
   ;; }
   ;;
   ;; create the zipper
-  (def zipper-domino-chain (partial z/zipper
-                                    (constantly true)
-                                    :children
-                                    #(assoc %1 :children %2)))
+(def zipper-domino-chain (partial z/zipper
+                                  (constantly true)
+                                  :children
+                                  #(assoc %1 :children %2)))
 
-  (defn create-domino-node [domino remain]
-    {:domino domino :remain remain :children []})
+(defn create-domino-node [domino remain]
+  {:domino domino :remain remain :children []})
 
-  ;; First we will build manually the tree
+(comment
+    ;; First we will build manually the tree
   ;; create the root location
   (def root-loc (zipper-domino-chain (create-domino-node [1 2] [[3 2] [4 5] [3 4] [2 7]])))
+
 
   ;; all chains can be built in one tree navigation. In the example below, 2 chains are built
   ;; and none is valid. Use z/next to deep-first navigate the tree until reach back the root (end? = true)
@@ -255,22 +297,98 @@
       (z/edit #(assoc % :last true)) ;; end of this chain
       z/next
       z/next
-      z/end?
-      ;;z/node
+      ;;z/end?
+      z/root
       ;;
       )
+;;
+  )
 
-  (defn place-dominoes [loc]
-    (let [{:keys [domino remain]} (z/node loc)]
-      (z/next (if (empty? remain)
-                loc
-                (-> loc
-                    (z/append-child (create-domino-node (first remain) (rest remain))))))))
+
+  ;; ...so we should be able to cretae the complete tree in one shot.
+
+(defn terminate-chain [loc]
+  (-> loc
+      (z/edit #(assoc % :last true))))
+
+(defn place-dominoes [loc]
+  (let [{:keys [domino remain]} (z/node loc)]
+    (z/next (if (empty? remain)
+              (terminate-chain loc)
+              (let [possible-connections (find-possible-connections domino remain)]
+                (if (empty? possible-connections)
+                  (terminate-chain loc)
+                  (loop [connections possible-connections
+                         loc loc]
+                    (if (empty? connections)
+                      (terminate-chain loc)
+                      (let [[connected-domino remaining-dominoes] (first connections)]
+                        (recur (rest connections)
+                               (-> loc
+                                   (z/append-child (create-domino-node connected-domino remaining-dominoes)))))))))))))
+
+(defn valid-chain-terminator [loc head-domino-val]
+  (let [{:keys [domino last remain]} (z/node loc)]
+    (and last
+         (empty? remain)
+         (= (second domino) head-domino-val))))
+
+(comment
+  (valid-chain-terminator {:domino [3 4] :last true} 4)
+  (valid-chain-terminator {:domino [3 5] :last true} 4)
+  (valid-chain-terminator {:domino [4 5] :last true} 4)
+  (valid-chain-terminator {:domino [6 5] :last true} 4))
+
+
+
+
+(comment
+  ;;(def root-loc (zipper-domino-chain (create-domino-node [1 2] [[2 3] [3 1] [2 4] [2 4]])))
+  ;;(def root-loc (zipper-domino-chain (create-domino-node [1 2] [[2 3] [3 1] [1 1] [2 2] [3 3]])))
+  ;;(def root-loc (zipper-domino-chain (create-domino-node [1 2] [[5 3] [3 1] [1 2] [2 4] [1 6] [2 3] [3 4] [5 6]])))
+  (def root-loc (zipper-domino-chain (create-domino-node [1 2] [[2 3] [3 1] [4 4]])))
 
 
   (z/root (last (take-while #(not (z/end? %))
                             (iterate place-dominoes root-loc))))
 
+  ;;(def placed-root-loc (zipper-domino-chain (z/root (last (take-while #(not (z/end? %))
+  ;;                                                                    (iterate place-dominoes root-loc))))))
+  (def placed-root-loc (->> (iterate place-dominoes root-loc)
+                            (take-while #(not (z/end? %)))
+                            last
+                            z/root
+                            zipper-domino-chain))
 
-  ;;
+  (z/root placed-root-loc)
+  (z/end? (last (take-while #(or (not (z/end? %))
+
+                                 (let [node (z/node %)]
+                                   (and (:last node)
+                                        (= 1 (last (:domino node)))))) (iterate z/next placed-root-loc))))
+
+
+  (->> (iterate z/next placed-root-loc)
+       (drop-while #(and (not (z/end? %))
+                         (not (valid-chain-terminator % 1))))
+       first
+       z/end?
+       ;;z/node
+       ;;z/path
+       ;;(map :domino)
+       ;;z/node
+       )
+
+;;
   )
+
+(defn create-all-chains [root-loc]
+  (->> (iterate place-dominoes root-loc)
+       (take-while #(not (z/end? %)))
+       last
+       z/root
+       zipper-domino-chain))
+
+(defn can-chain? [])
+
+
