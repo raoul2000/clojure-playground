5c1392dff909d3217653490c7e9e2bbdf4454c0f
raoul2000 <manu34@free.fr>
2022-07-11 22:37:40 +0200

    exercism: spiral-matrix just starting ...

---
+++ exercism/clojure/spiral-matrix/src/spiral_matrix.clj
@@ -0,0 +1,18 @@
+(ns spiral-matrix)
+
+;; 1 2 3
+;; 8 9 4
+;; 7 6 5
+;; min-y = 0
+;; max-y = 2
+;; min-x = 0
+;; max-x = 2 
+
+
+
+
+
+
+(defn spiral [n] ;; <- arglist goes here
+  ;; your code goes here
+)
