678e7e1bb506276568063b1bb6c36f7c8b2e929a
raoul2000 <manu34@free.fr>
2022-07-23 18:02:33 +0200

    test agent

--- pedestal/play-2/test/back/concurrent/file_access_test.clj
+++ pedestal/play-2/test/back/concurrent/file_access_test.clj
@@ -1,47 +1,45 @@
 (ns concurrent.file-access-test
   (:require [babashka.fs :as fs]
-            [clojure.core.async :as async]))
+            [clojure.core.async :as async]
+            [clojure.string :refer [join]]))
 
 ;; see https://stackoverflow.com/questions/6404717/idiomatic-file-locking-in-clojure
 
 (def work-dir "c:\\tmp\\concurrent")
 
-(defn write-to-file [n]
-  (let [content   (apply str (conj (repeat 50 (str n)) \newline))
-        file-path (str (fs/path work-dir "file-1.txt"))]
+
+;; first let's highlight issue when several threads are writting to the same file 
+
+(defn write-to-file [n file-path]
+  (let [content   (join "-" (conj (repeat 50 (str n)) \newline))]
     (println (format "%s" content))
-    (Thread/sleep 1000)
+    (Thread/sleep (rand-int 100))
     (spit file-path
           content
           :append true)
-    (flush)))
+    (Thread/sleep (rand-int 100))
+    (flush)
+    file-path))
 
-(defn print-to-stdout [n]
-  (prn n))
+;; each go block is scheduled for execution in an undetermined order. In the
+;; end, all lines are consistant but not written in order
 
-(def writer-chan (async/chan 100))
-
-
-
-(comment
-
-  (async/go 
-    (println (async/<! writer-chan) ))
-  (async/>!! writer-chan "hey")
-  
-  (spit (str (fs/path work-dir "file1.txt")) "some text")
-  (apply str (repeat 5 "e"))
-
-  (write-to-file 10)
-
-
-  (async/go
-    (prn "goo"))
-  (dotimes [n 9]
+(defn test-1 []
+  (dotimes [n 50]
     (async/go
-      (write-to-file n)
-      ;;(print-to-stdout n)
-     ))
+      (write-to-file n (str (fs/path work-dir "file-1.txt"))))))
 
-  ;;
-  )
+;; all lines are written in order
+
+(defn test-2 []
+  (let [file-agent (agent (str (fs/path work-dir "file-1.txt")))
+        watcher    (fn [k r os ns] (print "lll"))]
+    (add-watch file-agent :write watcher)
+    (dotimes [n 50]
+      (send-off file-agent (partial write-to-file n)))
+    (prn "await...")
+    (await file-agent)
+    (prn "done")))
+
+  ;;(shutdown-agents)
+
