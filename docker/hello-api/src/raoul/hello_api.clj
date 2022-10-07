2468ad5eff69819e0fc1b7f133f1e54bab1ca730
raoul2000 <manu34@free.fr>
2022-10-07 23:38:55 +0200

    docker: trying to use dockerized dev env

---
+++ docker/hello-api/src/raoul/hello_api.clj
@@ -0,0 +1,27 @@
+(ns raoul.hello-api
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route])
+  (:gen-class))
+
+
+(defn respond-hello [request]
+  {:status 200 :body "Hello, world!"})
+
+(def routes
+  (route/expand-routes
+   #{["/greet" :get respond-hello :route-name :greet]}))
+
+(defn create-server []
+  (http/create-server
+   {::http/routes routes
+    ::http/type   :jetty
+    ::http/port   8890}))
+
+(defn start []
+  (http/start (create-server)))
+
+
+(defn -main
+  [& args]
+  (println "starting server ...")
+  (start))
---
+++ docker/hello-api/test/raoul/hello_api_test.clj
@@ -0,0 +1,7 @@
+(ns raoul.hello-api-test
+  (:require [clojure.test :refer :all]
+            [raoul.hello-api :refer :all]))
+
+(deftest a-test
+  (testing "success !"
+    (is (= 1 1))))
