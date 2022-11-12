5f69f95b9b237609aabaed08e4662fb536d8e517
raoul2000 <manu34@free.fr>
2022-11-12 23:06:05 +0100

    codewar: update to deps.edn project

---
+++ codewar/test/codewar/codewar_test.clj
@@ -0,0 +1,7 @@
+(ns codewar.codewar-test
+  (:require [clojure.test :refer :all]
+            [codewar.codewar :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
--- codewar/test/codewar/sudoku_test.clj
+++
@@ -1,1 +0,0 @@
-(ns codewar.sudoku-test)
