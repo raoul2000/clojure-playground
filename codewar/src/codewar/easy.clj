ef971b024e9c398968e8a694a88599b7d778006e
raoul2000 <manu34@free.fr>
2022-11-17 23:02:26 +0100

    codewar: row-sum-odd-number - solution again in timeout !!

--- codewar/src/codewar/easy.clj
+++ codewar/src/codewar/easy.clj
@@ -51,24 +51,69 @@
 ;; l3 l4 : +6
 ;; l4 l5 : +8
 ;; l5 l6 : +10
-(defn row-sum-odd-numbers [row-num]
+(defn row-sum-odd-numbers-too-long-again [row-num]
   (let [start-at (apply + (into [1] (take (dec row-num) (drop 1 (iterate (partial + 2) 0)))))]
     (apply + (take row-num (iterate (partial + 2) start-at)))))
 
 (comment
-  
+
   (def row-num 4)
-  (into [1 ](take row-num (drop 1 (iterate (partial + 2) 0))))
-  (def start (apply + (into [1 ](take (dec row-num) (drop 1 (iterate (partial + 2) 0))))))
-  
+  (into [1] (take row-num (drop 1 (iterate (partial + 2) 0))))
+  (def start (apply + (into [1] (take (dec row-num) (drop 1 (iterate (partial + 2) 0))))))
+
   (take row-num (iterate (partial + 2) start))
   (take row-num (iterate (partial + 2) start))
-  
+
+  (row-sum-odd-numbers-too-long-again 1)
+  (row-sum-odd-numbers-too-long-again 5)
+  (row-sum-odd-numbers-too-long-again 7)
+  (time (row-sum-odd-numbers-too-long-again 2000000))
+
+  ;;
+  )
+
+;; let's try another implementation  faster than this one
+
+;;              1
+;;           3     5
+;;        7     9    11
+;;    13    15    17    19
+;; 21    23    25    27    29
+;; 31....
+
+;; row 1 starts at 1
+;; row 2 starts at 3
+;; row 3 starts at 7
+;; row 4 starts at 13
+;; row 5 starts at 21
+;; row 6 starts at 31
+;; row n starts at (n * (n-1)) + 1
+;; row n contain n items
+
+(defn row-sum-odd-numbers [row-num]
+  (let [start-from (inc (* row-num (dec row-num)))]
+    (->> start-from
+         (iterate (partial + 2))
+         (take row-num)
+         (apply +))))
+
+(comment
+  (def row-num 7)
+  (let [start-from (inc (* row-num (dec row-num)))]
+    (->> start-from
+         (iterate (partial + 2))
+         (take row-num)
+         (apply +)))
+
   (row-sum-odd-numbers 1)
   (row-sum-odd-numbers 5)
   (row-sum-odd-numbers 7)
   (time (row-sum-odd-numbers 2000000))
-  
+  (time (row-sum-odd-numbers Integer/MAX_VALUE))
+  (Integer/MAX_VALUE)
   ;;
   )
- 
+
+;; ok I don't understand why again, a timeout exception is thrown 
+;; message posted
+
