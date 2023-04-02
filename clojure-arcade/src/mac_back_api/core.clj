b194cdb2165c7ca364e08417c7cfee02a382a8d4
raoul2000 <manu34@free.fr>
2023-04-02 13:41:51 +0200

    clojure-arcade: pac man level 1

--- clojure-arcade/src/mac_back_api/core.clj
+++ clojure-arcade/src/mac_back_api/core.clj
@@ -8,9 +8,53 @@
 (defn move [direction]
   (go (>! mac-outgoing {:action :step :direction direction})))
 
+
+;; Level 0 ---------------------------------------------------
 (defn level-0 [data]
   (when (drop-while #(not (= "." %)) (first (:current data)))
     (move :right)))
+
+;; level 1 ----------------------------------------------------
+
+(defn at-position [x y matrix]
+  (-> matrix
+      (nth y)
+      (nth x)))
+
+(defn pacman-position [matrix]
+  (let [index (->> matrix
+                   flatten
+                   (keep-indexed #(when (= %2 "P") %1))
+                   first)
+        col-count (count (first matrix))]
+    [(mod index col-count) (quot index col-count)]))
+
+(defn at [[x y] matrix direction]
+  (let [dy (case direction
+             :up   (dec y)
+             :down (inc y)
+             x)
+        dx (case direction
+             :left  (dec x)
+             :right (inc x)
+             y)]
+    (at-position dx dy matrix)))
+
+(defn possible-steps [matrix]
+  (let [current-pos (pacman-position matrix)
+        item-at     (partial at current-pos matrix)
+        result (filter #(= "." (item-at %)) [:up :down :left :right])]
+    (print result)
+    result))
+
+
+(defn level-1 [data]
+  (->> data
+       possible-steps
+       first
+       move))
+
+;; Game Engine -----------------------------------------------------------
 
 (defn process-incoming []
   (go-loop []
@@ -19,22 +63,68 @@
         (println "Process incoming go loop terminated")
         (do
           (clojure.pprint/pprint data)
-          
-          (level-0 data)
+
+          (level-1 data)
 
           (println "Deeze Moves")
           (recur))))))
 (comment
+
+  (defn at-position [x y matrix]
+    (-> matrix
+        (nth y)
+        (nth x)))
+
+  (def m  [["■" "■" "■" "■" "■" "■" "■"]
+           ["■" "P" "." "." "." "." "■"]
+           ["■" "■" "■" "■" "■" "■" "■"]])
+
+  (at-position 2 1 m)
+
+  (keep-indexed #(when (= %2 "P") %1) (flatten [["X" "■" "■" "■" "■" "■" "■"]
+                                                ["■" "." "." "P" "." "." "■"]
+                                                ["■" "■" "■" "■" "■" "■" "■"]]))
+  (defn pacman-position [matrix]
+    (let [index (->> matrix
+                     flatten
+                     (keep-indexed #(when (= %2 "P") %1))
+                     first)
+          col-count (count (first matrix))]
+      [(mod index col-count) (quot index col-count)]))
+
+  (defn at [[x y] matrix direction]
+    (let [dy (case direction
+               :up   (dec y)
+               :down (inc y)
+               x)
+          dx (case direction
+               :left  (dec x)
+               :right (inc x)
+               y)]
+      (at-position dx dy matrix)))
+
+  (defn possible-steps [matrix]
+    (let [current-pos (pacman-position matrix)
+          item-at     (partial at current-pos matrix)]
+      (filter #(= "." (item-at %)) [:up :down :left :right])))
 
 
   (def m  [["■" "■" "■" "■" "■" "■" "■"]
            ["■" "P" "." "." "." "." "■"]
            ["■" "■" "■" "■" "■" "■" "■"]])
 
-  (second m)
-  (drop-while #(not (= "." %)) (first m))
-  (when (drop-while #(not (= "." %)) (first m))
-    (>! mac-outgoing {:action :step :direction :right})))
+  (possible-steps [["■" "." "■" "■" "■" "■" "■"]
+                   ["." "P" "." "." "." "." "■"]
+                   ["■" "." "■" "■" "■" "■" "■"]])
+
+  (pacman-position [["X" "■" "■" "■" "■" "■" "■"]
+                    ["■" "X" "." "X" "." "." "■"]
+                    ["■" "■" "■" "■" "■" "■" "P"]])
+
+
+
+  ;;
+  )
 
 (comment
   ;; === Step 1
@@ -55,6 +145,7 @@
   ;; Move the player
   ;; Valid :direction values are :up :down :left :right
   (go (>! mac-outgoing {:action :step :direction :right}))
+  (go (>! mac-outgoing {:action :step :direction :down}))
 
   ;; == Other functions
   ;; Iterate and improve how you are processing where to move the player.
@@ -66,6 +157,6 @@
 
   ;; Move, laugh, cry, get ended by ghosts and then start all over
   (go (>! mac-outgoing {:action :restart}))
-  
+
   ;;
   )
