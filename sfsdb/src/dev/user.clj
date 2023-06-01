cbd8318194ddca93f8835ed20c458c02be478ba5
raoul2000 <manu34@free.fr>
2023-06-01 10:55:30 +0200

    sfsdb: add morse dep

--- sfsdb/src/dev/user.clj
+++ sfsdb/src/dev/user.clj
@@ -8,3 +8,8 @@
 (add-tap #'p/submit)
 
 (tap> :hello) ; Start tapping out values
+
+;; TODO: setup morse https://github.com/nubank/morse/blob/main/docs/guide.adoc
+
+; Syntax error (UnsupportedClassVersionError) compiling new at (dev\nu\morse.clj:83:3).
+; javafx/embed/swing/JFXPanel has been compiled by a more recent version of the Java Runtime (class file version 55.0), this version of the Java Runtime only recognizes class file versions up to 52.0
