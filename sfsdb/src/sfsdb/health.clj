0870b7f47ee2db80902962f8aba5c8adfd045832
raoul2000 <manu34@free.fr>
2023-06-22 11:56:39 +0200

    sfsdb: comment health check module

--- sfsdb/src/sfsdb/health.clj
+++ sfsdb/src/sfsdb/health.clj
@@ -167,6 +167,10 @@
 (comment
   ;; another refacto
 
+  ;; - :help : a string describing the exam
+  ;; - :selected? : predicate function. argument is a Path. When it returns TRUE, the exam is applied to the item
+  ;; - :examine  : exam function that take one argument the Path to test and returns a result
+
   (def exams-2 {:exam-1 {:help       "ex1"
                          :selected?  (constantly true)
                          :examine    (constantly {:result "ok"})}
@@ -174,8 +178,8 @@
                          :selected?  (constantly true)
                          :examine    (constantly true)}
                 :size   {:help       "data file size"
-                          :selected? (constantly true)
-                          :examine   (constantly (rand-int 10))}})
+                         :selected? (constantly true)
+                         :examine   (constantly (rand-int 10))}})
 
   (defn run-single-exam [subject]
     (fn [exam-report [exam-id {:keys [selected? examine]}]]
@@ -185,12 +189,26 @@
                                                                     :result  (examine subject)})))
         exam-report)))
 
-  (defn examine [exams]
+  (defn apply-exams [exams]
     (fn [acc fs-path]
       (reduce (run-single-exam fs-path) acc (seq exams))))
 
   (def in-1 ["item-1" "item-2" "item-3"])
-  (reduce (examine exams-2) {} in-1)
+
+  (defn examine 
+    "Apply a map of exams to each item in *coll* and returns a exam result map
+     
+     - **key** : the exam id
+     - **value** : a coll of exams results represented as maps with 2 keys
+       - `:subject` : the item that was examined
+       - `:result` : the result of exam as returned by the `:examine` function
+     "
+    [coll exams]
+    (reduce (apply-exams exams) {} coll))
+  
+
+  (examine in-1 exams-2)
+
 
 
   ;;
