ae3d217fca7c9c4670e4698fb2c745caf0576a81
raoul2000 <manu34@free.fr>
2023-07-03 12:24:16 +0200

    sfsdb: fix exam and add new ones

--- sfsdb/src/sfsdb/health/core.clj
+++ sfsdb/src/sfsdb/health/core.clj
@@ -3,8 +3,6 @@
             [babashka.fs :as fs]
             [sfsdb.options :as opts]))
 
-
-
 ;; --------------------------
 
 (defn meta-file-for-data-file?
@@ -16,59 +14,39 @@
   (and (meta-file? path)
        (not= (fs/file-name path) (str "." (:metadata-extension opts/default)))))
 
-(comment
-
-
-  (defn data-file-name [meta-file-path]
-    (let [parent-dir (fs/parent meta-file-path)]
-      (->> meta-file-path
-           fs/file-name
-           fs/strip-ext
-           (fs/path parent-dir))))
-
-  (data-file-name "/a/z/.meta")
-  (data-file-name "/a/z/xxx.meta")
-  (data-file-name "/a/z/xxx.txt.meta")
-  (data-file-name "/a/z/xxx.txt")
-
-  (fs/strip-ext "/a/e/dddd.txt.meta")
-
-  (-> "/zz/.file.txt.meta" fs/strip-ext fs/exists?)
-
-
-  (fs/path (fs/cwd) "test/fixture/fs/root2/file2.txt.meta")
-
-  (-> (fs/path (fs/cwd) "test/fixture/fs/root2/file2.txt.meta") fs/strip-ext fs/exists?)
-  (-> (fs/path (fs/cwd) "test/fixture/fs/root2/file4.txt.meta") fs/strip-ext fs/exists?)
-
-
-
-  (hash-map :aa 22)
-
-  ;;
-  )
-
   ;; - :help : a string describing the exam
   ;; - :selected? : predicate function. argument is a Path. When it returns TRUE, the exam is applied to the item
   ;; - :examine  : exam function that take one argument the Path to test and returns a result
 
-(def exams-2 {:metadata-orphan {:help         "list all metadata files not related to a data file"
-                                :selected?    #(and (meta-file-for-data-file? %)
-                                                    (fs/regular-file? %))
-                                :examine      #(-> % fs/strip-ext fs/exists?)
-                                :accumulator  (fn [result-coll new-result]
-                                                (cond-> result-coll
-                                                  (not (:result new-result)) (conj result-coll new-result)))}
-              
-              :empty-data-file {:help         "Find all empty data files"
-                                :selected?    #(and (fs/regular-file? %)
-                                                    (not (meta-file? %)))
-                                :examine      #(zero? (fs/size %))
-                                :accumulator  (fn [result-coll new-result]
-                                                (cond-> result-coll
-                                                  (:result new-result) (conj result-coll new-result)))}
+(def exams-2 {:metadata-orphan      {:help         "list all metadata files not related to a data file"
+                                     :selected?    #(and (meta-file-for-data-file? %)
+                                                         (fs/regular-file? %))
+                                     :examine      #(-> % fs/strip-ext fs/exists?)
+                                     :accumulator  (fn [result-coll new-result]
+                                                     (cond-> result-coll
+                                                       (not (:result new-result)) (conj  new-result)))}
+
+              :empty-data-file       {:help         "Find all empty data files"
+                                      :selected?    #(and (fs/regular-file? %)
+                                                          (not (meta-file? %)))
+                                      :examine      #(zero? (fs/size %))
+                                      :accumulator  (fn [result-coll new-result]
+                                                      (cond-> result-coll
+                                                        (:result new-result) (conj  new-result)))}
+
+              :data-file-total-size {:help          "Calculate total Data file size"
+                                     :selected?     #(and (fs/regular-file? %)
+                                                          (not (meta-file? %)))
+                                     :examine       fs/size
+                                     :accumulator   (fn [total-size file-size]
+                                                      ((fnil + 0)  total-size (:result file-size)))}
+
+              :list-dir-content     {:help          "Count files or dirs in all directories"
+                                     :selected?     fs/directory?
+                                     :examine       #(count (fs/list-dir %))}
               ;;
               })
+
 
 
 ;; --------------------------
@@ -85,8 +63,6 @@
   (fn [acc fs-path]
     (reduce (run-single-exam fs-path) acc (seq exams))))
 
-(def in-1 ["item-1" "item-2" "item-3"])
-
 (defn examine
   "Apply a map of exams to each item in *coll* and returns a exam result map
      
@@ -98,8 +74,6 @@
   [coll exams]
   (reduce (apply-exams exams) {} coll))
 
-
-
 (comment
   ;; another refacto
 
@@ -109,12 +83,10 @@
 
   (map meta-file-for-data-file? (fs/glob root "**"))
 
-  
-  (map meta-file-for-data-file? (fs/glob (fs/path (fs/cwd) "test/fixture/fs") "**"))
+
+  (map meta-file-for-data-file? (fs/glob (fs/path (fs/cwd) "test/fixture/fs/root3") "**"))
 
   (examine (fs/glob (fs/path (fs/cwd) "test/fixture/fs") "**") exams-2)
-
-  ;; FIXME: unexpected results for exam :empty-data-file
 
 ;;
   )
