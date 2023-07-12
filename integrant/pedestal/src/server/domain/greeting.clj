62878cfb598e125ffb47e113e8525a674b9e78ce
raoul2000 <manu34@free.fr>
2023-07-12 18:59:57 +0200

    improve integrant sample

--- integrant/pedestal/src/server/domain/greeting.clj
+++ integrant/pedestal/src/server/domain/greeting.clj
@@ -5,7 +5,7 @@
 (defn greeting-for [name]
   (tap> name)
   (cond
-    (nil? name)            "hello, stranger :)"
+    (nil? name)            "hello, stranger !"
     (#{"bob" "max"} name)  nil
     :else                  (str "hello, " (if (s/blank? name)
                                             "mysterious"
--- pedestal/play-1/dev/user.clj
+++ pedestal/play-1/dev/user.clj
@@ -9,7 +9,9 @@
   (set-refresh-dirs "src")
 
   (def srv (server/start-dev))
-
+;; #function[io.pedestal.interceptor/eval7439/fn--7440/fn--7441],
+  
+  
   (server/stop-dev srv)
   (refresh)
 
