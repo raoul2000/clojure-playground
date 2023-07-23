fd5b80ee066d3838d8cf2509fbfd0960feb644bd
raoul2000 <manu34@free.fr>
2023-07-23 23:39:57 +0200

    exercism: poker

--- exercism/clojure/poker/src/poker_2.clj
+++ exercism/clojure/poker/src/poker_2.clj
@@ -1,45 +1,46 @@
-(ns poker
+(ns poker-2
   (:require [clojure.string :refer [split join]]))
 
-(defn normalize-card [^String card]
-  (let [[_ rank suit] (re-matches #"([0-9JQK]+)(.)" card)
-        figure-vals   {"J" 11
-                       "Q" 12
-                       "K" 13}]
-    (try
-      [(or (figure-vals rank)
-           (Integer. rank))
-       suit]
-      (catch Exception e
-        (throw (ex-info (str "failed to normalize card " card)
-                        {:cause (.getMessage e)}))))))
+(def suit-key {"H" :heart
+               "S" :spade
+               "C" :club
+               "D" :diamond})
+
+(def figure-vals {"J" 11
+                  "Q" 12
+                  "K" 13})
+
+(defn normalize-card [card]
+  (let [[_ rank suit] (re-matches #"([0-9JQK]+)(.)" card)]
+    [(or (figure-vals rank)
+         (Integer. rank))
+     (get suit-key suit)]))
 
 (comment
   (normalize-card "3H")
   (normalize-card "JH")
-  (normalize-card "2S")
-  (normalize-card "2C")
   (normalize-card "KH")
   (normalize-card "10H")
   (normalize-card "AC")
   ;;
   )
 
+(defn sort-by-rank-desc [hand]
+  (reverse (sort-by first hand)))
 
 (defn normalize-hand
-  "Given a string representing a list of cards, returns a vector, 
-   where each item is represents a card as a pair [rank suit]"
+  "Given a string representing a list of cards, returns a vector in descending order
+   by card rank, where each card is represented as a pair [rank suit]"
   [s]
   (->> (split s #" ")
-       (map normalize-card)))
+       (map normalize-card)
+       (sort-by-rank-desc)))
 
 (comment
   (normalize-hand "4S 3D")
-  (normalize-hand "4S 3D KH 3C")
+  (normalize-hand "JE 4S 3D KH")
   ;;
   )
-
-(defn hand->string [v])
 
 (defn pair-count
   "Given a *hand* returns the number of pair(s)"
@@ -52,7 +53,8 @@
        (count)))
 
 (comment
-  (pair-count [[1 "E"] [3 "B"] [2 "R"]])
+  (pair-count [[1 :e] [3 :b] [2 :r]])
+  (pair-count [[1 :e] [2 :b] [1 :r]])
   (pair-count [[1 :e] [2 :b] [1 :r] [2 :t]])
   ;;
   )
@@ -127,33 +129,18 @@
   )
 
 (defn straight-flush? [hand]
-  (and (straight? hand)
-       (flush? hand)))
+ (and (straight? hand)
+      (flush? hand)) )
 
 (comment
   (straight-flush? [[5 :a] [4 :a] [3 :a] [2 :a] [1 :a]])
   (straight-flush? [[5 :a] [4 :a] [3 :a] [2 :a] [1 :b]])
-
-  (sort ["4S 5S 7H 8D JC"])
   ;;
   )
 
 ;; -----------------
 
-(defn compare-hands [hand-a hand-b]
-  1)
-
-(defn best-hands [hands]
-  (if (= 1 (count hands))
-    [(first hands)]
-    (sort-by normalize-hand compare-hands hands )))
-
 (comment
-  (defn f [xs ys] (= (sort (best-hands xs)) (sort ys)))
-  (sort (best-hands  ["4D 5S 6S 8D 3C"
-                     "2S 4C 7S 9H 10H"
-                     "3S 4S 5D 6H JH"]))
-  (sort ["4S 5S 7H 8D JC"])
   
-  ;;
+  (normalize-hand "4S 5H 5S 5D 5C")
   )
