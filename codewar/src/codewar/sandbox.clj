8eab411923bff1d062c508bee4856039cc8c3929
raoul2000 <manu34@free.fr>
2024-08-06 19:17:51 +0200

    codewar: easy one

---
+++ codewar/src/codewar/sandbox.clj
@@ -0,0 +1,14 @@
+(ns codewar.sandbox
+  (:require [clojure.string :as s]))
+
+;; https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/clojure
+
+(defn solution [strng ending]
+  ; TODO
+  )
+
+(comment
+  (s/ends-with?  )
+  
+  ;;
+  )
